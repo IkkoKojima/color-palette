930dae00d5964cb0b6d6ae4a1dc2b08889ae7345
i_kojima <ikko.kojima@septeni-original.co.jp>
2020-03-20 16:14:58 +0900

    フロントのリクエスト形式の変更に伴う修正 formdata -> json

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sklearn.cluster import KMeans
 import numpy as np
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import base64
 
 app = Flask(__name__)
 CORS(app)
@@ -15,9 +16,10 @@ def ping():
 @app.route('/', methods=['POST'])
 def gen_color_palette_from_image():
     try:
-        filestr = request.files["file"].read()
-        npimg = np.fromstring(filestr, np.uint8)
-        image = cv2.imdecode(npimg, cv2.IMREAD_UNCHANGED)
+        img_base64 = request.get_json()["image64"]
+        img_data = base64.b64decode(img_base64)
+        img_np = np.fromstring(img_data, np.uint8)
+        image = cv2.imdecode(img_np, cv2.IMREAD_UNCHANGED)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = image.reshape((image.shape[0] * image.shape[1], 3))
         clt = KMeans(n_clusters = 3)
